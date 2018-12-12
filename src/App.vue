7165656d8fab4a8accb0f4e7557478e8564104d9
andrew.challen <andrew.challen@local>
2018-12-12 13:02:27 +0000

    demo pwa app

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+
+body {
+  padding: 2em;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>PWA</h1>
+    <p>{{counter}}</p>
+    <button @click="bump">Bump</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      counter: 1
+    }
+  },
+  methods: {
+    bump () {
+      this.counter++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
